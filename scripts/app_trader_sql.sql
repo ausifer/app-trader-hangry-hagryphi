bd931148ebdca67b8791305f086a560199ac0f02
ausifer <austin.bradley.ward@gmail.com>
2021-08-21 13:58:05 -0500

    got the target table and pulled it together for summary by name but still issues

--- scripts/app_trader_sql.sql
+++ scripts/app_trader_sql.sql
@@ -1,94 +1,83 @@
-/* WITH apple AS (SELECT * FROM app_store_apps),
-droid AS (SELECT * FROM play_store_apps)
-SELECT * FROM apple
-SELECT * FROM 
-ORDER BY name;
-
-SELECT -- This subquery helps to put most columns together from App / Play stores. 
-	name,
-	primary_genre AS "category",
-	rating,
-	size_bytes AS "size",
-	content_rating,
-	price,
--- 	CAST(price AS int),
--- 	0 AS install_count,
-	'App Store' AS "store"
-FROM app_store_apps
-UNION ALL
-SELECT 
-	name,
-	category,
-	rating,
-	size,
-	content_rating,
-	nullif(regexp_replace(price, '[^0-9.]*','','g'), '')::numeric as cost,
--- 	CAST(install_count AS int),
-	'Play Store' AS "store"
-FROM play_store_apps; */
-
-
--- EVERYTHING ABOVE THIS POINT IS AUSTIN'S QUERY - BUT WE'RE USING HOLLAND'S BASIC STRUCTURE BECAUSE HE FIGURED THINGS OUT QUICKER AND BETTER
-
-WITH apple AS (
-	SELECT * FROM app_store_apps
-	where rating >= 4
-	AND price > 0
-	),
+-- This is a query to find all apps in both the App Store and Play Store with a rating >= 4 and price > 0, their category,
+-- expected lifespan in years, lifetime earnings, lifetime cost, net profit and total net profit for both stores
+--  CTE to select app store table
+WITH apple AS ( 
+    SELECT * 
+    FROM app_store_apps
+    WHERE rating >= 4
+    AND price > 0),
+-- CTE to select play store table
 google AS (
-	SELECT * FROM play_store_apps
-	WHERE rating >= 4
-	AND price != '0'
-	)
-SELECT *   -- THIS NEEDS TO BE A CTE AS WELL AND WE NEED TO GET RID OF CATEGORIES BESIDES GAMES
-FROM
-(SELECT
-	name,
-	rating,
-	store,
-	cost,
-	category, 	
-	ROUND(rating / .5,3) + 1 AS expected_lifespan_in_years,
-	rating * 2 + 1 * 12 * 5000 AS earnings_per_mo, --THIS IS STILL WRONG FOR MINE, SO THE OTHER CALCULATIONS BELOW ARE WRONG, TOO
-	CASE WHEN cost <= 1 THEN 10000
-		WHEN cost > 1 THEN cost * 10000
-		END AS purchase_cost,
-	CASE WHEN cost <= 1 THEN 10000 + (ROUND(rating / .5,1) + 1 * 12 * 1000)
-		WHEN cost > 1 THEN cost * 10000 + (ROUND(rating / .5,1) + 1 * 12 * 1000)
-		END AS total_marketing_cost,
-	(CASE WHEN cost <= 1 THEN 10000
-		WHEN cost > 1 THEN cost * 10000
-		END) + 
- 	(CASE WHEN cost <= 1 THEN 10000 + (ROUND(rating / .5,1) + 1 * 12 * 1000)
-		WHEN cost > 1 THEN cost * 10000 + (ROUND(rating / .5,1) + 1 * 12 * 1000)
-		END) AS total_cost,
- 	((rating * 2 + 1 * 12 * 5000)*(ROUND(rating / .5,3) + 1)) - 
- 	((CASE WHEN cost <= 1 THEN 10000
-		WHEN cost > 1 THEN cost * 10000
-		END) + 
- 	(CASE WHEN cost <= 1 THEN 10000 + (ROUND(rating / .5,1) + 1 * 12 * 1000)
-		WHEN cost > 1 THEN cost * 10000 + (ROUND(rating / .5,1) + 1 * 12 * 1000)
-		END)) AS net_profit
-FROM
-(
-	SELECT name,
-		rating,
-		'Apple' AS store,
-		price AS cost,
-		primary_genre AS "category"
-		FROM apple
-		WHERE name IN (SELECT name FROM google)
+    SELECT * 
+    FROM play_store_apps
+    WHERE rating >= 4
+    AND price != '0'
+    ),
+-- subquery to determine columns and total net profit
+target AS (SELECT DISTINCT TRIM(name) AS name,
+        rating,
+        store,
+        cost,
+        TRIM(INITCAP(
+			CASE WHEN category ILIKE '%Game%' THEN 'Games'
+				WHEN category ILIKE '%Health%' THEN 'Health & Fitness'
+				WHEN category ILIKE '%Food%' THEN 'Food & Drink'
+			ELSE category END 
+		)) AS category,
+        expected_lifespan_in_years,
+        lifetime_earnings,
+        lifetime_cost,
+        net_profit,
+SUM(net_profit) OVER(PARTITION BY name) AS total_net_profit
+-- FROM clause to pick select columns from each table and UNION into one
+FROM (SELECT DISTINCT name,
+    rating,
+    'Apple' AS store,
+    price AS cost,
+    TRIM(INITCAP(primary_genre)) AS "category",
+    ((rating * 2) + 1) AS expected_lifespan_in_years,
+    CAST((((rating * 2) + 1) * 12 * 2500) AS money) AS lifetime_earnings,
+    CAST((CASE WHEN price <= 1 THEN 10000 + (((rating * 2) + 1) * 12 * 500)
+        WHEN price > 1 THEN price * 10000 + (((rating * 2) + 1) * 12 * 500)
+        END) AS MONEY) AS lifetime_cost,
+    CAST((((rating * 2) + 1) * 12 * 2500) - (CASE WHEN price <= 1 THEN 10000 + (((rating * 2) + 1) * 12 * 500)
+        WHEN price > 1 THEN price * 10000 + (((rating * 2) + 1) * 12 * 500)
+        END) AS MONEY) AS net_profit
+FROM apple
+WHERE name IN (SELECT name FROM google)
+UNION ALL
+SELECT DISTINCT name,
+    rating,
+    'Google' AS store,
+    NULLIF(regexp_replace(price, '[^0-9.]*','','g'), '')::numeric AS cost,
+    category,
+    ((rating * 2) + 1) + 1 AS expected_lifespan_in_years,
+    CAST((((rating * 2) + 1) * 12 * 2500) AS MONEY) AS lifetime_earnings,
+    CAST((CASE WHEN NULLIF(regexp_replace(price, '[^0-9.]*','','g'), '')::numeric <= 1 THEN 10000 + (((rating * 2) + 1) * 12 * 500)
+        WHEN NULLIF(regexp_replace(price, '[^0-9.]*','','g'), '')::numeric > 1 THEN NULLIF(regexp_replace(price, '[^0-9.]*','','g'), '')::numeric * 10000 + (((rating * 2) + 1) * 12 * 500)
+        END) AS MONEY)lifetime_cost,
+    CAST((((rating * 2) + 1) * 12 * 2500) - (CASE WHEN NULLIF(regexp_replace(price, '[^0-9.]*','','g'), '')::numeric <= 1 THEN 10000 + (((rating * 2) + 1) * 12 * 500)
+        WHEN NULLIF(regexp_replace(price, '[^0-9.]*','','g'), '')::numeric > 1 THEN NULLIF(regexp_replace(price, '[^0-9.]*','','g'), '')::numeric * 10000 + (((rating * 2) + 1) * 12 * 500)
+        END) AS MONEY) AS net_profit
+FROM google
+WHERE name IN (SELECT name FROM apple)) AS sub
+-- ORDER BY total net profit to show the most profitable first, then by name to group the names together.
+ORDER BY total_net_profit DESC, TRIM(name))
 
-		UNION ALL
-	SELECT 	name,
-		rating,
-		'Google' AS store,
-		NULLIF(regexp_replace(price, '[^0-9.]*','','g'), '')::numeric AS cost,
-		category
-	FROM google
-		WHERE name IN (SELECT name FROM play_store_apps)
-		ORDER BY name
-) AS combo
-ORDER BY net_profit DESC) as test
-;
+-- Pulling the data all together by name
+SELECT 
+	DISTINCT name AS name,
+	COUNT(DISTINCT category) 				AS category_count,
+	CASE WHEN COUNT(store) >= 3 THEN 2
+	WHEN COUNT(store) < 3 THEN COUNT(store) 
+	END										AS store_count,
+	AVG(rating) AS avg_rating,
+	AVG(expected_lifespan_in_years) 		AS avg_lifespan,
+    SUM(lifetime_earnings) 					AS combined_lifetime_earnings,
+    SUM(lifetime_cost) 						AS combined_lifetime_cost,
+    SUM(net_profit) 						AS total_net_profit
+FROM target
+GROUP BY name
+ORDER BY total_net_profit DESC;
+
 
